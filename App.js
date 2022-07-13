a03caea23e1fbcfba79f90a9219426865b319f9b
brady.underwood <brady.underwood@envestnet.com>
2022-07-13 12:42:38 -0400

    basic tab navigation

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const Learn = () => (
+  <View style={styles.learn}>
+  <CustomText>Learn</CustomText>
+  <StatusBar style="auto" />
+</View>
+);
+
+const Plan = () => (
+  <View style={styles.plan}>
+  <CustomText>Plan</CustomText>
+  <StatusBar style="auto" />
+</View>
+)
+
+const Invest = () => (
+  <View style={styles.invest}>
+  <CustomText>Invest</CustomText>
+  <StatusBar style="auto" />
+</View>
+);
+
+const CustomText = ({children}) => (
+  <Text style={{fontSize:50}}>{children}</Text>
+);
+
+const Tab = createBottomTabNavigator();
+
+const AppNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Learn" component={Learn}/>
+    <Tab.Screen name="Plan" component={Plan}/>
+    <Tab.Screen name="Invest" component={Invest}/>
+  </Tab.Navigator>
+);
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer>
+			<AppNavigator />
+		</NavigationContainer>
+	);
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  learn: {
+		flex: 1,
+		backgroundColor: 'blue',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	plan: {
+		flex: 1,
+		backgroundColor: 'green',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+  invest: {
+		flex: 1,
+		backgroundColor: 'red',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
