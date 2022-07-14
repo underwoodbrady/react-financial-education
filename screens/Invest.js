67d5c5d57d0a84f865d0e4c1019ff5b4de817c8a
Brady Underwood <underwood.brady@gmail.com>
2022-07-14 00:29:51 -0400

    merged code in invest screen

--- screens/Invest.js
+++ screens/Invest.js
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Alert, Image } from "react-native";
 import CustomText from "../components/CustomText";
 
 import { useNavigation } from "@react-navigation/native";
@@ -8,56 +7,35 @@ import { useNavigation } from "@react-navigation/native";
 const Invest = () => {
     const nav = useNavigation();
     return (
-        <View style={styles.container}>
+        <View style={styles.invest}>
             <CustomText>Invest</CustomText>
+            <Text>Hello there!</Text>
+            <StatusBar style="auto" />
             <Button
-                title="Go to profile"
+                title="Go to Profile"
                 onPress={() => nav.navigate("Profile")}
             />
-            <StatusBar style="auto" />
+            <Button
+                title="Press me"
+                onPress={() => Alert.alert("Thanks for clicking me!")}
+            />
+            <Image
+                source={{
+                    uri: "https://reactnative.dev/docs/assets/p_cat2.png",
+                }}
+                style={{ width: 200, height: 200 }}
+            />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
+    invest: {
         flex: 1,
-        backgroundColor: "tan",
+        backgroundColor: "yellow",
         alignItems: "center",
         justifyContent: "center",
     },
-=======
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , Button, Alert, Image} from 'react-native';
-import CustomText from '../components/CustomText';
-
-const Invest = () => (
-	<View style={styles.invest}>
-		<CustomText>Invest</CustomText>
-		<Text>Hello there!</Text>
-		<StatusBar style="auto" />
-		<Button
-        title="Press me"
-        onPress={() => Alert.alert('Thanks for clicking me!')}
-      />
-	  <Image
-          source={{
-            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-          }}
-          style={{ width: 200, height: 200 }}
-        />
-	</View>
-
-);
-
-const styles = StyleSheet.create({
-	invest: {
-		flex: 1,
-		backgroundColor: 'yellow',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
->>>>>>> 4a84eeaee0222aa125a0c0bc95239b67b61ba168
 });
 
 export default Invest;
