4a84eeaee0222aa125a0c0bc95239b67b61ba168
Cameron Marcus <cammarcus19@gmail.com>
2022-07-14 00:06:10 -0400

    logo added and a small update to invest.js

--- screens/Invest.js
+++ screens/Invest.js
@@ -1,18 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Button, Alert, Image} from 'react-native';
 import CustomText from '../components/CustomText';
 
 const Invest = () => (
 	<View style={styles.invest}>
 		<CustomText>Invest</CustomText>
+		<Text>Hello there!</Text>
 		<StatusBar style="auto" />
+		<Button
+        title="Press me"
+        onPress={() => Alert.alert('Thanks for clicking me!')}
+      />
+	  <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{ width: 200, height: 200 }}
+        />
 	</View>
+
 );
 
 const styles = StyleSheet.create({
 	invest: {
 		flex: 1,
-		backgroundColor: 'red',
+		backgroundColor: 'yellow',
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
