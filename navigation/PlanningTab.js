d8ae2acb52f1b403fdfed7980f4cf8f045c1a160
brady.underwood <brady.underwood@envestnet.com>
2022-07-20 13:48:48 -0400

    minor style changes to planning tab

--- navigation/PlanningTab.js
+++ navigation/PlanningTab.js
@@ -1,8 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-import { FontAwesome5 } from '@expo/vector-icons'; 
-
+import { FontAwesome5 } from '@expo/vector-icons';
 
 import Budgets from '../screens/Budgets';
 import Goals from '../screens/Goals';
@@ -10,17 +9,20 @@ import Goals from '../screens/Goals';
 const Tab = createMaterialTopTabNavigator();
 
 const PlanningTab = () => (
-	<Tab.Navigator screenOptions={{
-		tabBarStyle: {backgroundColor:"#0A1E2D"},
-		tabBarLabelStyle: { fontSize: 15 },
-		tabBarItemStyle:{height:60},
-		headerShown:false,
-		tabBarInactiveTintColor:'#B6C5D4',
-		tabBarActiveTintColor: '#69ABE6',
-		tabBarIndicatorStyle: {borderColor:"red", color:"red"},
-	}}>
-		<Tab.Screen name="Budgets" component={Budgets}/>
-		<Tab.Screen name="Goals" component={Goals}/>
+	<Tab.Navigator
+		screenOptions={{
+			tabBarStyle: { backgroundColor: '#0A1E2D' },
+			tabBarLabelStyle: { fontSize: 15, fontWeight:"600" },
+			tabBarItemStyle: { height: 60 },
+			tabBarIndicatorStyle: { backgroundColor: 'red' },
+			headerShown: false,
+			tabBarInactiveTintColor: '#B6C5D4',
+			tabBarActiveTintColor: '#69ABE6',
+			tabBarIndicatorStyle: { borderColor: 'red', color: 'red' },
+		}}
+	>
+		<Tab.Screen name="Budgets" component={Budgets} />
+		<Tab.Screen name="Goals" component={Goals} />
 	</Tab.Navigator>
 );
 
