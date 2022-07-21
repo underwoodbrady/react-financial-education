e62c7192fdf9c091afc85b05b9b6f3912109dece
Cameron Marcus <cammarcus19@gmail.com>
2022-07-21 10:12:08 -0400

    hearts added

--- screens/IntroQuiz.js
+++ screens/IntroQuiz.js
@@ -4,6 +4,7 @@ import { Button, Text, View, StyleSheet } from 'react-native';
 import CustomText from '../components/CustomText';
 import Svg, { Rect, Circle } from 'react-native-svg';
 import QuizHeader from "../components/learn/QuizHeader";
+import { AntDesign } from '@expo/vector-icons'; 
 
 
 
@@ -12,12 +13,15 @@ const Quiz = () => {
 const [currentQuestion, setCurrentQuestion] = useState(0)
 const [showScore, setShowScore] = useState(false)
 const [score, setScore] = useState(0)
-const numQuestions = questions.length//TODO: map.length
+const numQuestions = questions.length
+const [numIncorrect,setIncorrect] = useState(0)
 //below is where we can add animations for correct/incorrect answers etc
 const handleAnswerButtonClick = (isCorrect) => {
     if (isCorrect === true) {
       setScore(score + 1);
       questions[currentQuestion].questionAnsweredRight = 1;
+    } else {
+      setIncorrect(numIncorrect + 1);
     }
 
     const nextQuetions = currentQuestion + 1;
@@ -29,19 +33,37 @@ const handleAnswerButtonClick = (isCorrect) => {
       setShowScore(true)
     }
   }
+  //do open or closed hearts
+  const heartsIncorrect  = [
+    <View style={{flexDirection: 'row'}}>
+      <AntDesign name="heart" size={24} color="red" />
+      <AntDesign name="heart" size={24} color="red" />
+      <AntDesign name="heart" size={24} color="red" />
+    </View>,  
+    <View style={{flexDirection: 'row'}}>
+      <AntDesign name="heart" size={24} color="red" />
+      <AntDesign name="heart" size={24} color="red" />
+      <AntDesign name="hearto" size={24} color="red" />
+    </View>,  
+    <View style={{flexDirection: 'row'}}>
+      <AntDesign name="heart" size={24} color="red" />
+      <AntDesign name="hearto" size={24} color="red" />
+      <AntDesign name="hearto" size={24} color="red" />
+    </View> ]
 
   return (
 
+    
     <View style={styles.container}>
     {showScore ?
         <Text>You scored {score} out of {questions.length}</Text>
         :
         (
         <>
-          <View style={{marginTop:0, marginBottom:16}}><QuizHeader score={score} numQuestions = {numQuestions}/></View>
             <View>
-                <Text>Question {currentQuestion + 1} out of {questions.length} </Text>
-                <CustomText>{questions[currentQuestion].questionText}</CustomText>
+            <View><QuizHeader score={score} numQuestions = {numQuestions}/></View>
+              {heartsIncorrect[numIncorrect]}
+              <CustomText>{questions[currentQuestion].questionText}</CustomText>
             </View>
             <View className='answer-section'>
                 <Text>
@@ -62,7 +84,7 @@ const handleAnswerButtonClick = (isCorrect) => {
 
 const styles = StyleSheet.create({
 	container: {
-    backgroundColor: '#01101B',
+    backgroundColor: 'blue',
     padding: 32,
 	},
 });
