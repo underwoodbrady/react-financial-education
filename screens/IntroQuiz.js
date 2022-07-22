31af0dd4014a1af3192ce54812f9f722acc8f0af
Cameron Marcus <cammarcus19@gmail.com>
2022-07-22 13:07:54 -0400

    Quiz added with bug

--- screens/IntroQuiz.js
+++ screens/IntroQuiz.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import questions from '../Data/IntroQuizData';
-import { Button, Text, View, StyleSheet } from 'react-native';
+import { Button, Text, View, StyleSheet, Vibration } from 'react-native';
 import CustomText from '../components/CustomText';
 import Svg, { Rect, Circle } from 'react-native-svg';
 import QuizHeader from '../components/learn/QuizHeader';
 import QuizButton from '../components/learn/QuizButton';
 import { AntDesign } from '@expo/vector-icons';
+//shuffle here maybe
 
 const Quiz = () => {
 	const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -13,24 +14,76 @@ const Quiz = () => {
 	const [score, setScore] = useState(0);
 	const numQuestions = questions.length;
 	const [numIncorrect, setIncorrect] = useState(0);
+  const [incorrectOne,setIncorrectOne] = useState(0);
+  const [incorrectTwo,setIncorrectTwo] = useState(0);
+  const [visitedOne,setVisitedOne] = useState(0);
+  const [visitedTwo,setVisitedTwo] = useState(0);
+  
+
 	//below is where we can add animations for correct/incorrect answers etc
 	const handleAnswerButtonClick = (isCorrect) => {
 		if (isCorrect === true) {
 			setScore(score + 1);
-			questions[currentQuestion].questionAnsweredRight = 1;
 		} else {
       //right here we can get mad about 3 incorrect maybe
-			setIncorrect(numIncorrect + 1);
+      if (numIncorrect == 0) {
+        setIncorrectOne(currentQuestion);
+      } else if (numIncorrect == 1) {
+        setIncorrectTwo(currentQuestion);
+      } else if (numIncorrect == 2) {
+        setShowScore(true);
+      }
+      setIncorrect(numIncorrect + 1);
 		}
 
-		const nextQuetions = currentQuestion + 1;
+		//const nextQuestions = currentQuestion + 1;
+    if (score != numQuestions) {
+      if (currentQuestion + 1 < questions.length && visitedOne == 0 && visitedTwo == 0) {
+        setCurrentQuestion(currentQuestion + 1);
+      } else if (numIncorrect > 0 && visitedOne == 0) {
+        setCurrentQuestion(incorrectOne);
+        setVisitedOne(1);
+      } else if (numIncorrect == 2 && visitedTwo == 0) {
+        setCurrentQuestion(incorrectTwo);
+        setVisitedTwo(1);
+      } 
+    } else {
+        setShowScore(true);
+      }
 
-		if (nextQuetions < questions.length) {
-			setCurrentQuestion(nextQuetions);
+  }
+
+  //restart function,
+  //when icon is pushed, we must reset variables: score, currentQuestion
+    /*
+		if (nextQuestions < questions.length) {
+			setCurrentQuestion(nextQuestions);
 		} else {
-			setShowScore(true);
+			  setShowScore(true);
 		}
-	};
+	}; */
+
+  const shuffleArray = (array) => {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+};
+
+  const failProtocol = () => {
+
+  };
+
+  const winProtocol = () => {
+
+  };
+
+  const restartQuiz = () => {
+
+  };
+
 	//do open or closed hearts
 	const heartsIncorrect = [
 		<View style={{ flexDirection: 'row' }}>
@@ -52,11 +105,15 @@ const Quiz = () => {
 	];
 
 	return (
+    //shuffle here
 		<View style={styles.container}>
 			{showScore ? (
 				<Text style={styles.scoreText}>
 					You scored {score} out of {questions.length}
+          {numIncorrect} the inc: {incorrectOne}
+          {/*games or resources are suggested here*/}
 				</Text>
+        //instead we call the win or lose protocol to display what we want
 			) : (
 				<>
 					<View>
@@ -72,7 +129,7 @@ const Quiz = () => {
 							}
 						</View>
 						<Text style={styles.questionText}>
-							{questions[currentQuestion].questionText}
+							{questions[currentQuestion].questionText} score {score} currentQuestion {currentQuestion} inc1 {incorrectOne} inc2 {incorrectTwo} numInc {numIncorrect}
 						</Text>
 					</View>
 					<View>
